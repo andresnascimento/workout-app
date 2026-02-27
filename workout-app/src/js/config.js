83d17d3308951bc07789d0e8b34f4c0abcc8ae15
André Nascimento <asncontato@gmail.com>
2026-02-27 15:05:43 -0300

    bug fix

--- workout-app/src/js/config.js
+++ workout-app/src/js/config.js
@@ -1,4 +1,4 @@
 const spreadsheetId = "1vl8j8trlq4WvrlbA2ilAS8-aJ_JWDoiggMN9PB_i71g";
-const apiKey = "AIzaSyBmXc6unLgEKRSoyaI4BoVhCsjsb43PAKI";
+const apiKey = import.meta.env.VITE_API_KEY;
 const range = "workout!A1:G";
 export const workoutURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
